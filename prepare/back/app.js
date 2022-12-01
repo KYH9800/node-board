f80bb0fb64b6de1e1bbac2776dc6931bbb14c3e1
고윤혁 <sksgur3217@gmail.com>
2022-12-01 21:53:47 +0900

    add morgan

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+// morgan
+const morgan = require('morgan');
 // cors
 let cors = require('cors');
 // index.js in router
@@ -12,6 +14,8 @@ connect();
 // 클라이언트로 부터 받은 http 요청 메시지 형식에서 body데이터를 해석하기 위함
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('combined'));
 
 // cors
 app.use(
